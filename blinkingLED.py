a36f932550168754438f9dc1288e2e8055836708
Ediel Lopez <ediellopez2@gmail.com>
2019-05-21 19:13:32 -0700

    Blinking LED

---
+++ blinkingLED.py
@@ -0,0 +1,30 @@
+# BLINKING LED
+import RPi.GPIO as GPIO
+import time
+
+LedPin = 21    # pin 21
+
+def setup():
+	GPIO.setmode(GPIO.BCM)       # Numbers GPIOs by physical location
+	GPIO.setup(LedPin, GPIO.OUT)   # Set LedPin's mode is output
+	GPIO.output(LedPin, GPIO.HIGH) # Set LedPin high(+3.3V) to off led
+
+def loop():
+	while True:
+		print ('...led on')
+		GPIO.output(LedPin, GPIO.LOW)  # led on
+		time.sleep(0.5)
+		print ('led off...')
+		GPIO.output(LedPin, GPIO.HIGH) # led off
+		time.sleep(0.3)
+
+def destroy():
+	GPIO.output(LedPin, GPIO.HIGH)     # led off
+	GPIO.cleanup()                     # Release resource
+
+if __name__ == '__main__':     # Program start from here
+	setup()
+	try:
+		loop()
+	except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child program destroy() will be  executed.
+		destroy()
